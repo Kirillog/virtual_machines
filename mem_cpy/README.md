5622cded5628bd4ead4a08dd3e5423a84976001f
Kirill Mitkin <kirill.mitkin.03@gmail.com>
2025-04-12 23:20:47 +0300

    mem_cpy: add thread pool

--- mem_cpy/README.md
+++ mem_cpy/README.md
@@ -11,13 +11,13 @@ cmake --build build --target memcpy
 ```sh
 ./build/mem_cpy/memcpy
 
-Pool size: 0, time: 30931 microseconds
-Pool size: 1, time: 26719 microseconds
-Pool size: 2, time: 26579 microseconds
-Pool size: 3, time: 24693 microseconds
-Pool size: 4, time: 25453 microseconds
-Pool size: 5, time: 24480 microseconds
-Pool size: 6, time: 24526 microseconds
-Pool size: 7, time: 24161 microseconds
-Pool size: 8, time: 24341 microseconds
+Pool size: 0, time: 30201 microseconds
+Pool size: 1, time: 26210 microseconds
+Pool size: 2, time: 23843 microseconds
+Pool size: 3, time: 23636 microseconds
+Pool size: 4, time: 24479 microseconds
+Pool size: 5, time: 24185 microseconds
+Pool size: 6, time: 24376 microseconds
+Pool size: 7, time: 24504 microseconds
+Pool size: 8, time: 24836 microseconds
 ```
--- mem_cpy/main.cpp
+++ mem_cpy/main.cpp
@@ -1,24 +1,95 @@
 #include <cassert>
 #include <chrono>
+#include <condition_variable>
 #include <cstring>
+#include <deque>
+#include <functional>
+#include <mutex>
 #include <random>
 #include <thread>
 
 static constexpr size_t MEM_SIZE = 256 * 1024 * 1024;
 
+typedef std::function<void()> task_t;
+
+class ThreadPool {
+private:
+    void Run() {
+        while (true) {
+            std::unique_lock lock{task_mutex_};
+            while (!closed_ && tasks_.empty()) {
+                not_empty_var_.wait(lock);
+            }
+ 
+            if (closed_) {
+                break;
+            }
+            auto task = std::move(tasks_.front());
+    
+            tasks_.pop_front();
+            lock.unlock();
+            task();
+            std::this_thread::yield();
+        }
+    
+    }
+public:
+    ThreadPool(size_t pool_size) {
+        threads_.reserve(pool_size);
+        for (size_t i = 0; i < pool_size; ++i) {
+            threads_.emplace_back(&ThreadPool::Run, this);
+        }
+    }
+
+    void WaitShutdown() {
+        {
+            std::lock_guard lock{task_mutex_};
+            if (closed_) {
+                return;
+            }
+            closed_ = true;
+        }
+
+        for (size_t i = 0; i < threads_.size(); ++i) {
+            not_empty_var_.notify_one();
+        }    
+    
+        for (auto& thread : threads_) {
+            thread.join();
+        }    
+    }
+
+    void Submit(task_t&& task) {
+        std::unique_lock lock{task_mutex_};
+        if (closed_) {
+            return;
+        }
+        tasks_.emplace_back(std::move(task));
+        not_empty_var_.notify_one();
+    }
+
+private:
+    std::mutex task_mutex_;
+    bool closed_{false};
+    std::vector<std::thread> threads_;
+    std::deque<task_t> tasks_;
+    std::condition_variable not_empty_var_;
+};
+
 void* parallel_memcpy(void* dst, const void* src, size_t size, size_t pool_size) {
-    std::vector<std::jthread> pool_threads;
+    ThreadPool pool{pool_size};
     size_t thread_count = pool_size + 1;
     size_t csize = size / thread_count;
     for (size_t i = 0; i < pool_size; ++i) {
         char * dfrom = (char *)dst + csize * i;
         char * sfrom = (char *)src + csize * i;
-        pool_threads.emplace_back([dfrom, sfrom, csize](){
+        pool.Submit([dfrom, sfrom, csize](){
             memcpy(dfrom, sfrom, csize);
         });
     }
     size_t offset = csize * pool_size;
     memcpy((char *)dst + offset, (char *)src + offset, size - offset);
+    pool.WaitShutdown();
     return dst;
 }
 
