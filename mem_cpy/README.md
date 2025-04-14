0c1272dae3a9dacac1995ecd86f11931a5b503d4
Kirill Mitkin <kirill.mitkin.03@gmail.com>
2025-04-14 16:29:27 +0300

    mem_cpy: add thread pool reuse

--- mem_cpy/README.md
+++ mem_cpy/README.md
@@ -11,13 +11,14 @@ cmake --build build --target memcpy
 ```sh
 ./build/mem_cpy/memcpy
 
-Pool size: 0, time: 37273 microseconds
-Pool size: 1, time: 26673 microseconds
-Pool size: 2, time: 23934 microseconds
-Pool size: 3, time: 24440 microseconds
-Pool size: 4, time: 24182 microseconds
-Pool size: 5, time: 23898 microseconds
-Pool size: 6, time: 23683 microseconds
-Pool size: 7, time: 23712 microseconds
-Pool size: 8, time: 23508 microseconds
+Default memcpy time:     23758 microseconds
+Pool size: 0, time:      28343 microseconds
+Pool size: 1, time:      23150 microseconds
+Pool size: 2, time:      21848 microseconds
+Pool size: 3, time:      22449 microseconds
+Pool size: 4, time:      22229 microseconds
+Pool size: 5, time:      21889 microseconds
+Pool size: 6, time:      21637 microseconds
+Pool size: 7, time:      21691 microseconds
+Pool size: 8, time:      21917 microseconds
 ```
--- mem_cpy/main.cpp
+++ mem_cpy/main.cpp
@@ -1,20 +1,41 @@
 #include <cassert>
 #include <chrono>
+#include <condition_variable>
 #include <cstring>
+#include <mutex>
 #include <random>
 #include <thread>
 
 static constexpr size_t MEM_SIZE = 256 * 1024 * 1024;
 
-typedef void *(*memcpy_func_t)(void *, const void *, size_t);
-
+static std::mt19937 rnd;
 static size_t pool_size;
 
 class MemcpyPool {
 private:
     void Run(size_t i, void* dst, const void* src, size_t size) {
-        size_t csize = size / (pool_size + 1);
-        memcpy((char *)dst + csize * i, (char *)src + csize * i, csize);
+        size_t local_run_step = 0;
+        while (true) {
+            ++local_run_step;
+            {
+                std::unique_lock lock{mutex_};
+                tpool_var_.wait(lock, [&](){
+                    return closed_ || run_step_ == local_run_step;
+                });
+            }
+            if (closed_) {
+                break;
+            }
+            size_t csize = size / (pool_size + 1);
+            memcpy((char *)dst + csize * i, (char *)src + csize * i, csize);
+            {
+                std::unique_lock lock{mutex_};
+                ++completed_;
+                if (completed_ == pool_size) {
+                    manager_var_.notify_one();
+                }
+            }
+        }
     }
 public:
     MemcpyPool(void* dst, const void* src, size_t size, size_t pool_size) {
@@ -24,23 +45,43 @@ public:
         }
     }
 
-    void WaitShutdown() {
-        for (auto& thread : threads_) {
-            thread.join();
-        }    
+    void Start() {
+        std::unique_lock lock{mutex_};
+        ++run_step_;
+        tpool_var_.notify_all();
+    }
+
+    void WaitStep() {
+        std::unique_lock lock{mutex_};
+        manager_var_.wait(lock, [&](){
+            return completed_ == pool_size;
+        });
+        completed_ = 0;
+    }
+
+    ~MemcpyPool() {
+        std::unique_lock lock{mutex_};
+        closed_ = true;
+        tpool_var_.notify_all();
     }
 
 private:
-    std::vector<std::thread> threads_;
+    std::vector<std::jthread> threads_;
+    std::mutex mutex_;
+    std::condition_variable tpool_var_;
+    std::condition_variable manager_var_;
+    size_t run_step_{0};
+    size_t completed_{0};
+    bool closed_{false};
 };
 
-void* parallel_memcpy(void* dst, const void* src, size_t size) {
+void* parallel_memcpy(MemcpyPool& pool, void* dst, const void* src, size_t size) {
+    pool.Start();
     size_t offset = (size / (pool_size + 1)) * pool_size;
     memcpy((char *)dst + offset, (char *)src + offset, size - offset);
+    pool.WaitStep();
     return dst;
 }
-
-static std::mt19937 rnd;
 
 void init_mem(char *ptr, size_t size) {
     for (char *ptr_end = ptr + size; ptr < ptr_end; ptr += 8) {
@@ -52,15 +93,21 @@ int main() {
     std::vector<char> smem(MEM_SIZE);
     std::vector<char> dmem(MEM_SIZE);
     init_mem(smem.data(), smem.size());
+    init_mem(dmem.data(), dmem.size());
+    auto from = std::chrono::steady_clock::now();
+    memcpy(dmem.data(), smem.data(), smem.size());
+    auto to = std::chrono::steady_clock::now();
+    auto time =  std::chrono::duration_cast<std::chrono::microseconds>(to - from).count();
+    assert(memcmp(smem.data(), dmem.data(), smem.size()) == 0);
+    fprintf(stderr, "Default memcpy time:\t %ld microseconds\n", time);
     for (pool_size = 0; pool_size <= 8; ++pool_size) {
         init_mem(dmem.data(), dmem.size());
         MemcpyPool pool{dmem.data(), smem.data(), smem.size(), pool_size};
-        auto from = std::chrono::steady_clock::now();
-        parallel_memcpy(dmem.data(), smem.data(), smem.size());
-        pool.WaitShutdown();
-        auto to = std::chrono::steady_clock::now();
-        auto time =  std::chrono::duration_cast<std::chrono::microseconds>(to - from).count();
-        printf("Pool size: %lu, time: %ld microseconds\n", pool_size, time);
+        from = std::chrono::steady_clock::now();
+        parallel_memcpy(pool, dmem.data(), smem.data(), smem.size());
+        to = std::chrono::steady_clock::now();
+        time =  std::chrono::duration_cast<std::chrono::microseconds>(to - from).count();
+        fprintf(stderr, "Pool size: %lu, time:\t %ld microseconds\n", pool_size, time);
         assert(memcmp(smem.data(), dmem.data(), smem.size()) == 0);
     }
     return 0;
